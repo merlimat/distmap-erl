4f08b49744ce3d524db7a1854dbec82771656fd1
Matteo Merli <mm@merlimat.org>
2009-03-17 14:24:21 +0100

    First try at processing incoming packets.

--- src/rudp.erl
+++ src/rudp.erl
@@ -17,7 +17,11 @@
 %% Flags:  0 : SYN - Active on first packet
 %%         1 : ACK - The ACK field is valid
 %%         2 : LAST - The packet is last in sequence
-%%         2 : ...
+%%         3 : REQ - The packet is last in sequence
+%%         4 : RESP - The packet is last in sequence
+%%         5 : ...
+%%         6 : ...
+%%         7 : ...
 
 %%
 %% Include files
@@ -39,15 +43,17 @@
 %% API Functions
 %%
 
--define( MTU, 1420 ).
+-define( MTU, 1412 ).
 
-create( Port ) ->
+create( Port ) when is_integer(Port) ->
 	create( {0,0,0,0}, Port, [] ).
 
 create( Address, Port, Options ) ->
 	log:start(),
 	gen_server:start_link ( ?MODULE, [Address, Port, Options], []).
 
+send( Pid, Peer, Data ) when is_binary(Data) ->
+	gen_server:cast(Pid, {send, Peer, Data});
 send( Pid, Peer, Data ) ->
 	Bin = term_to_binary(Data),
 	gen_server:cast(Pid, {send, Peer, Bin}).
@@ -56,14 +62,27 @@ send( Pid, Peer, Data ) ->
 %% Server functions
 %% ====================================================================
 
--record(state, { socket, nounce_cnt=0 }).
+-record(state, { socket, 
+				 nounce_cnt = 0, 
+				 operations = dict:new() }).
+
+-record(operation, {
+				nounce,
+				sender = remote,
+				pkt_list = [], 
+				last_ack = 0
+		} ).
+
 -record(pkt, {
 		syn = 0,
 		ack = 0,
 		last = 0,
+		req = 0,
+		resp = 0,
 		nounce,
 		sequence = 1,
-		ack_seq = 0
+		ack_seq = 0,
+		data
 	} ).
 
 init([Addr, Port, _Options]) ->
@@ -84,7 +103,7 @@ handle_call(_Request, _From, State) ->
     {reply, Reply, State}.
 
 handle_cast({send, {Addr,Port}, Data}, State) ->
-	{PktList, NewState} = prepare_data( Data, State ),
+	{PktList, NewState} = prepare_data( request, Data, State ),
 	lists:foreach( fun(Pkt) ->
 			         ok = gen_udp:send( State#state.socket, Addr, Port, Pkt )
 		           end, PktList ),
@@ -92,10 +111,10 @@ handle_cast({send, {Addr,Port}, Data}, State) ->
 
 handle_info( {udp, Socket, IP, Port, Packet},
              State=#state{ socket = Socket }) ->
-	?DEBUG_( "Got packet from ~s", util:format_addr(IP, Port) ),
-	{Pkt,_Data} = read_packet( Packet ),
-	io:format( "~s~n", [format_pkt(Pkt)] ),
-	{ noreply, State };
+	Pkt = read_packet( Packet ),
+	?DEBUG_( "Got packet from ~s - ~s",	[util:format_addr(IP, Port), format_pkt(Pkt)] ),
+	{ok, NewState} = process_fragment( {IP,Port}, Pkt, State ),
+	{ noreply, NewState };
 
 handle_info(_Info, State) ->
     {noreply, State}.
@@ -114,39 +133,54 @@ code_change(_OldVsn, State, _Extra) ->
 
 %% Packet creation
 
-prepare_data( Data, State ) when byte_size(Data) =< ?MTU ->
+prepare_data( request, Data, State ) ->
+	prepare_data( 1, Data, State );
+prepare_data( response, Data, State ) ->
+	prepare_data( 0, Data, State );
+
+prepare_data( Req, Data, State ) when byte_size(Data) =< ?MTU ->
 	Nounce = State#state.nounce_cnt + 1,
-	Pkt = new_packet( #pkt{ syn=1, last=1, nounce=Nounce }, Data ),
+	Resp = 1 - Req,
+	Pkt = new_packet( #pkt{ syn=1, last=1, req=Req, 
+							resp=Resp, nounce=Nounce, data=Data } ),
 	{ [Pkt], State#state{ nounce_cnt = Nounce } };
 
-prepare_data( Data, State ) ->
+prepare_data( Req, Data, State ) ->
 	Nounce = State#state.nounce_cnt + 1,
-	<<DataFirst:?MTU/binary, DataRest/binary>> = Data,
-	FirstPkt = new_packet( #pkt{ syn=1, nounce=Nounce }, DataFirst ),
-	Rest = prepare_data_cont( 2, DataRest, Nounce ),
+	Resp = 1 - Req,
+	{DataFirst, DataRest} = split_binary(Data, ?MTU),
+	FirstPkt = new_packet( #pkt{ syn=1, req=Req, 
+								 resp=Resp, nounce=Nounce, data=DataFirst } ),
+	Rest = prepare_data_cont( Req, 2, Nounce, DataRest ),
 	{ [FirstPkt|Rest], State#state{ nounce_cnt = Nounce } }.
 
-prepare_data_cont( Seq, Nounce, Data ) ->
-	List = prepare_data_cont( Seq, Nounce, Data, [] ),
+prepare_data_cont( Req, Seq, Nounce, Data ) ->
+	List = prepare_data_cont( Req, Seq, Nounce, Data, [] ),
 	lists:reverse( List ).
 
-prepare_data_cont( Seq, Nounce, Data, Acc ) when byte_size(Data) =< ?MTU ->
-	Pkt = new_packet( #pkt{ last=1, sequence=Seq, nounce=Nounce }, Data ), 
+prepare_data_cont( Req, Seq, Nounce, Data, Acc ) when byte_size(Data) =< ?MTU ->
+	Resp = 1 - Req,
+	Pkt = new_packet( #pkt{ last=1, sequence=Seq, req=Req, 
+							resp=Resp, nounce=Nounce, data=Data } ), 
 	[Pkt|Acc];
-prepare_data_cont( Seq, Nounce, Data, Acc ) ->
-	<<DataCurr:?MTU/binary, DataRest/binary>> = Data,
-	Pkt = new_packet( #pkt{ sequence=Seq, nounce=Nounce }, DataCurr ),
-	prepare_data_cont( Seq+1, Nounce, DataRest, [Pkt|Acc] ).
+prepare_data_cont( Req, Seq, Nounce, Data, Acc ) ->
+	Resp = 1 - Req,
+	{DataCurr, DataRest} = split_binary(Data, ?MTU),
+	Pkt = new_packet( #pkt{ sequence=Seq, req=Req, 
+							resp=Resp, nounce=Nounce, data=DataCurr } ),
+	prepare_data_cont( Req, Seq+1, Nounce, DataRest, [Pkt|Acc] ).
 
-new_packet( Header, DataFragment ) ->
- 	<< (Header#pkt.syn):1, 
- 	   (Header#pkt.ack):1,
-	   (Header#pkt.last):1,
-	   0:5, % pad with zeros
- 	   (Header#pkt.nounce):24,
-	   (Header#pkt.sequence):16,
- 	   (Header#pkt.ack_seq):16,
- 	   DataFragment/binary
+new_packet( Pkt ) ->
+ 	<< (Pkt#pkt.syn):1, 
+ 	   (Pkt#pkt.ack):1,
+	   (Pkt#pkt.last):1,
+	   (Pkt#pkt.req):1,
+	   (Pkt#pkt.resp):1,
+	   0:3, % pad with zeros
+ 	   (Pkt#pkt.nounce):24,
+	   (Pkt#pkt.sequence):16,
+ 	   (Pkt#pkt.ack_seq):16,
+ 	   (Pkt#pkt.data)/binary
  	>>.
 	   
 
@@ -156,18 +190,71 @@ read_packet( Bin ) ->
 	<< SYN:1, 
  	   ACK:1,
 	   LAST:1,
-	   0:5, % pad with zeros
+	   REQ:1,
+	   RESP:1,
+	   0:3, % pad with zeros
  	   Nounce:24,
 	   Sequence:16,
  	   AckSeq:16,
  	   DataFragment/binary
  	>> = Bin,
-	{ #pkt{ syn = SYN, ack = ACK, last = LAST,
+	#pkt{ syn = SYN, ack = ACK, req = REQ, resp = RESP, last = LAST,
 		  nounce = Nounce, sequence = Sequence, 
-		  ack_seq = AckSeq }, DataFragment }.
+		  ack_seq = AckSeq, data=DataFragment }.
 
 format_pkt( Pkt=#pkt{} ) ->
 	lists:flatten( io_lib:format( 
-		"[SYN:~p ACK:~p LAST:~p Nounce:~p Seq:~p Ack:~p]", 
+		"[SYN:~p ACK:~p LAST:~p REQ:~p RESP:~p Nounce:~p Seq:~p Ack:~p Size:~p]", 
 			[Pkt#pkt.syn, Pkt#pkt.ack, Pkt#pkt.last,
-			 Pkt#pkt.nounce, Pkt#pkt.sequence, Pkt#pkt.ack_seq]) ).
+			 Pkt#pkt.req, Pkt#pkt.resp,
+			 Pkt#pkt.nounce, Pkt#pkt.sequence, 
+			 Pkt#pkt.ack_seq, byte_size(Pkt#pkt.data)]) ).
+
+%% Incoming packet processing 
+
+process_fragment( From, Pkt=#pkt{req=1}, State ) ->
+	Dict = State#state.operations,
+	Nounce = Pkt#pkt.nounce,
+	Key = {From, Nounce},
+	Operation = case dict:find(Key, Dict) of 
+							{ok, Value} -> Value; 
+							error  ->  #operation{nounce=Nounce}
+					   end,
+	
+	PktList = sort_packets( [Pkt|Operation#operation.pkt_list] ),
+	io:format( "Is pkt_list complete? ~p~n", [is_pkt_list_complete(PktList)] ),
+	Operation1 = Operation#operation{pkt_list=PktList},
+	NewDict = dict:store( Key, Operation1, Dict ),
+	{ok, State#state{operations=NewDict}};
+
+process_fragment( From, Pkt=#pkt{resp=1}, State ) ->
+	{ok, State}.
+
+process_ack( Nounce, AckSeq, State ) ->
+	ok.
+
+% Sort a list of packets according to sequence number
+sort_packets( PktList ) ->
+	lists:sort( fun(A,B) -> 
+					if A#pkt.sequence > B#pkt.sequence -> false;
+						true -> true
+					end
+				end, PktList ).
+
+% Tells whether a packet list is complete 
+is_pkt_list_complete( PktList ) ->
+	is_pkt_list_complete(PktList, 1).
+
+is_pkt_list_complete( [Pkt], N ) ->
+	if Pkt#pkt.sequence == N -> 
+		   if Pkt#pkt.last == 1 -> true;
+		   	  true -> false
+		   end;
+	   true -> false
+	end;
+is_pkt_list_complete( [First|Rest], N ) ->
+	if First#pkt.sequence == N ->
+		   is_pkt_list_complete(Rest, N+1);
+	   true -> false
+	end.
+
